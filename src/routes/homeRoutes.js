4b52176ce5875d17367cea88cd27afe0ea5b9da4
jeanguanabara <jeanguanabara@gmail.com>
2023-01-26 18:58:09 -0300

    iniciando teste multer

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const HomeController = require('../controllers/HomeController')
+const multer = require('multer')//aqui importo o multer
 
 
 router.get('/', HomeController.showHome)
@@ -8,6 +9,8 @@ router.get('/', HomeController.showHome)
 router.get('/projeto1Bootstrap', HomeController.showProjeto1Bootstrap)
 router.get('/projeto2Bootstrap', HomeController.showProjeto2Bootstrap)
 router.get('/projeto3Bootstrap', HomeController.showProjeto3Bootstrap)
+
+
 
 
 
